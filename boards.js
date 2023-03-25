bc8917d59ac2b479aa863827b06b43bad6f6df0b
Dys <chemistryfrancium@gmail.com>
2023-03-25 16:56:53 +0800

    Initial commit

---
+++ boards.js
@@ -0,0 +1,210 @@
+function shuffle(array) {
+	let currentIndex = array.length,  randomIndex;
+	while (currentIndex != 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+		[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+	}
+
+	return array;
+}
+
+const game = {
+	board: new Map(),
+	source: null,
+	won: false,
+	makeWin() {
+		game.won = true;
+		document.getElementById("win-text").style.visibility = "visible";
+		for (const [_, poly] of game.board) {
+			poly.locked = true;
+			poly.lastCanvasUpdate = currentCanvasUpdate;
+		}
+	},
+	calcLight() {
+		const hasLight = new Map();
+		const hasCycle = new Map();
+		for (const [key, poly] of game.board) {
+			poly.search.visited = 0;
+			hasLight.set(key, poly.hasLight);
+			hasCycle.set(key, poly.hasCycle);
+			poly.hasLight = false;
+			poly.hasCycle = false;
+		}
+		function DFS(u, parent) {
+			u.search.visited = true;
+			for (let i = 0; i < u.sides; i++) {
+				const v = u.connections[i];
+				if (v === parent) continue;
+				if (!v || !u.hasConnection(i)) continue;
+				if (!v.hasConnection(v.connections.indexOf(u))) continue;
+				if (v.search.visited) {
+					v.hasCycle = true;
+					u.hasCycle = true;
+				} else {
+					v.hasLight = true;
+					DFS(v, u);
+				}
+			}
+		}
+		game.source.hasLight = 1;
+		DFS(game.source, game.source);
+		let allLit = true;
+		for (const [key, poly] of game.board) {
+			if (!poly.hasLight || poly.hasCycle) allLit = false;
+			if (hasLight.get(key) !== poly.hasLight) poly.lastCanvasUpdate = currentCanvasUpdate;
+		}
+		if (allLit) game.makeWin();
+	},
+	generateFromBoard() {
+		game.won = false;
+		document.getElementById("win-text").style.visibility = "hidden";
+		const NODE_PROBABILITY = 0.07;
+		const EDGE_PROBABILITY = 0.11;
+		const queue = new Queue();
+		for (const [_key, poly] of game.board) {
+			if (Math.random() < NODE_PROBABILITY || poly === game.source) queue.push(poly);
+		}
+		for (const [_key, poly] of game.board) {
+			if (poly.dsu.root !== poly || poly.dsu.size > 1) continue;
+			queue.push(poly);
+			while (queue.length()) {
+				const top = queue.pop();
+				let validCons = top.connections.map((other, idx) => [other, idx])
+					.filter(con => con[0] && con[0].dsu_find() !== top.dsu_find());
+				if (!validCons.length) continue;
+				if (queue.length === 0 || top === poly) { 
+					let rng = Math.floor(Math.random() * validCons.length);
+					top.addConnection(validCons[rng][1]);
+					validCons[rng][0].addConnection(validCons[rng][0].connections.indexOf(top));
+					top.dsu_union(validCons[rng][0]);
+					queue.push(validCons[rng][0]);
+				}
+				for (const con of validCons) {
+					if (Math.random() < EDGE_PROBABILITY && con[0].dsu_find() !== top.dsu_find()) {
+						top.addConnection(con[1]);
+						con[0].addConnection(con[0].connections.indexOf(top));
+						top.dsu_union(con[0]);
+						queue.push(con[0]);
+					}
+				}
+			}
+		}
+		if (game.source.dsu.size === game.board.size) return;
+		function DFS(u) {
+			u.search.visited = true;
+			const order = shuffle(Array.from(Array(u.sides), (_, i) => i));
+			for (const i of order) {
+				if (u.connections[i] && !u.connections[i].search.visited) {
+					DFS(u.connections[i]);
+					if (u.dsu_find() !== u.connections[i].dsu_find()) {
+						u.addConnection(i);
+						u.connections[i].addConnection(u.connections[i].connections.indexOf(u));
+						u.dsu_union(u.connections[i]);
+					}
+				}
+			}
+		}
+		DFS(game.source);
+	},
+	generateGame(type, x, y) {
+		game.board = new Map();
+		BOARD_TYPES[type].generate(x, y);
+		game.generateFromBoard();
+		for (const [_, poly] of game.board) {
+			poly.pipesRotation = Math.floor(Math.random() * poly.sides);
+			poly.pipesRotationDisplay = poly.pipesRotation;
+		}
+		game.calcLight();
+		initBoard();
+	}
+};
+
+const BOARD_TYPES = {
+	square: {
+		generate(x, y) {
+			for (let i = 0; i < x; i++) {
+				for (let j = 0; j < y; j++) {
+					game.board.set(i * y + j, new Polygon(4, 32, [i * 32 + 16, j * 32 + 16]));
+				}
+			}
+			game.source = game.board.get(Math.floor(x / 2) * y + Math.floor(y / 2));
+			for (let i = 0; i < x; i++) {
+				for (let j = 0; j < y; j++) {
+					const poly = game.board.get(i * y + j);
+					poly.connections[0] = game.board.get((i + 1) * y + j);
+					poly.connections[1] = game.board.get(i * y + (j - 1));
+					poly.connections[2] = game.board.get((i - 1) * y + j);
+					poly.connections[3] = game.board.get(i * y + (j + 1));
+				}
+			}
+		}
+	},
+	triangle: {
+		generate(x, y) {
+			const keyFrom = (x, y) => x.toString() + "_" + y.toString();
+			const sideLen = 50;
+			const midptR = sideLen / Math.sqrt(3) / 2;
+			const diam = sideLen * Math.sqrt(3) / 2;
+			for (let j = 0; j < y; j++) {
+				const subtr = ((j === y - 1) && (y % 2 === 1)) ? 1 : 0;
+				for (let i = 0 + subtr; i < 2 * x - 1 - subtr; i++) {
+					game.board.set(
+						keyFrom(i, j),
+						new Polygon(3, sideLen,
+							[(i + 1) * sideLen / 2, j * diam + midptR * (2 - (i + j) % 2)],
+							(i + j) % 2 === 0 ? Math.PI / 6 : -Math.PI * 5 / 6
+						)
+					);
+				}
+			}
+			game.source = game.board.get(keyFrom(x - 1, Math.floor(y / 2)));
+			for (const [key, poly] of game.board) {
+				let [x1, y1] = key.split("_");
+				x1 = parseInt(x1, 10);
+				y1 = parseInt(y1, 10);
+				const type = (y1 + x1) % 2;
+				poly.connections[type] = game.board.get(keyFrom(x1 + 1, y1));
+				poly.connections[1 - type] = game.board.get(keyFrom(x1 - 1, y1));
+				poly.connections[2] = game.board.get(keyFrom(x1, y1 + (type === 0 ? 1 : -1)));
+			}
+		}
+	},
+	hexagon: {
+		generate(x, y) {
+			const keyFrom = (x, y) => x.toString() + "_" + y.toString();
+			const sideLen = 24;
+			const midptR = sideLen / 2 / Math.tan(Math.PI / 6);
+			for (let i = 0; i < y; i++) {
+				for (let j = -Math.min(i, y - 1 - i); j < x + Math.min(i, y - 1 - i); j++) {
+					game.board.set(
+						keyFrom(i * 2, j * 2 + 1), 
+						new Polygon(6, sideLen, [(j + 1) * midptR * 2, i * sideLen * 3 + sideLen])
+					);
+				}
+			}
+			for (let i = 0; i < y - 1; i++) {
+				for (let j = -Math.min(i, y - 2 - i); j < x + 1 + Math.min(i, y - 2 - i); j++) {
+					game.board.set(
+						keyFrom(i * 2 + 1, j * 2), 
+						new Polygon(6, sideLen, [j * midptR * 2 + midptR, i * sideLen * 3 + sideLen * 2.5])
+					);
+				}
+			}
+			for (const [key, poly] of game.board) {
+				let [y1, x1] = key.split("_");
+				x1 = parseInt(x1, 10);
+				y1 = parseInt(y1, 10);
+				poly.connections[0] = game.board.get(keyFrom(y1, x1 + 2));
+				poly.connections[1] = game.board.get(keyFrom(y1 - 1, x1 + 1));
+				poly.connections[2] = game.board.get(keyFrom(y1 - 1, x1 - 1));
+				poly.connections[3] = game.board.get(keyFrom(y1, x1 - 2));
+				poly.connections[4] = game.board.get(keyFrom(y1 + 1, x1 - 1));
+				poly.connections[5] = game.board.get(keyFrom(y1 + 1, x1 + 1));
+			}
+			game.source = game.board.get(keyFrom(y - 1, Math.ceil(x / 2) * 2 - ((y % 2 === 1) ? 1 : 0)));
+		}
+	}
+}
+
+window.onload = () => requestAnimationFrame(() => game.generateGame("square", 5, 5));
---
+++ canvas.js
@@ -0,0 +1,129 @@
+const canvas = document.getElementById("c"), ctx = canvas.getContext("2d");
+
+function renderPolygon(polygon) {
+	if (currentCanvasUpdate - polygon.lastCanvasUpdate > 2) return;
+	ctx.fillStyle = polygon.locked ? "#888" : "#bbb";
+	ctx.strokeStyle = "#000";
+	ctx.lineWidth = 1;
+	ctx.beginPath();
+	ctx.moveTo(polygon.vertices[0][0], polygon.vertices[0][1]);
+	for (let i = 1; i <= polygon.sides; i++) {
+		ctx.lineTo(polygon.vertices[i % polygon.sides][0], polygon.vertices[i % polygon.sides][1]);
+	}
+	ctx.fill();
+	ctx.stroke();
+	let connections = 0;
+	for (let i = 0; i < polygon.sides; i++) {
+		if ((polygon.pipes & (1 << i)) === 0) continue;
+		connections++;
+	}
+	ctx.fillStyle = "#000";
+	ctx.beginPath();
+	const circleSize = polygon === game.source ? 10 : (connections > 1 ? 2 : 7);
+	ctx.arc(polygon.position[0], polygon.position[1], circleSize, 0, 2 * Math.PI);
+	ctx.fill();
+	ctx.strokeStyle = "#000";
+	ctx.lineWidth = 4;
+	ctx.beginPath();
+	const [x, y] = polygon.position;
+	for (let i = 0; i < polygon.sides; i++) {
+		if ((polygon.pipes & (1 << i)) === 0) continue;
+		ctx.moveTo(x, y);
+		let v = [polygon.midpts[i][0] - x, polygon.midpts[i][1] - y];
+		// Rotation matrix
+		const ang = -polygon.pipesRotationDisplay * 2 * Math.PI / polygon.sides;
+		const cosang = Math.cos(ang), sinang = Math.sin(ang);
+		v = [v[0] * cosang - v[1] * sinang, v[0] * sinang + v[1] * cosang];
+		ctx.lineTo(v[0] + x, v[1] + y);
+	}
+	ctx.stroke();
+	if (polygon.hasLight) {
+		ctx.beginPath();
+		ctx.lineWidth = 2;
+		ctx.strokeStyle = polygon.hasCycle ? "#f00" : "#0f0";
+		for (let i = 0; i < polygon.sides; i++) {
+			if ((polygon.pipes & (1 << i)) === 0) continue;
+			ctx.moveTo(x, y);
+			let v = [polygon.midpts[i][0] - x, polygon.midpts[i][1] - y];
+			// Rotation matrix
+			const ang = -polygon.pipesRotationDisplay * 2 * Math.PI / polygon.sides;
+			const cosang = Math.cos(ang), sinang = Math.sin(ang);
+			v = [v[0] * cosang - v[1] * sinang, v[0] * sinang + v[1] * cosang];
+			ctx.lineTo(v[0] + x, v[1] + y);
+		}
+		ctx.stroke();
+	}
+	if (polygon === game.source) {
+		ctx.fillStyle = "#000";
+		ctx.beginPath();
+		ctx.arc(polygon.position[0], polygon.position[1], 10, 0, 2 * Math.PI);
+		ctx.fill();
+		ctx.strokeStyle = "#0f0";
+		ctx.lineWidth = 3;
+		ctx.beginPath();
+		ctx.arc(polygon.position[0], polygon.position[1], 6, 0, 2 * Math.PI);
+		ctx.stroke();
+	} else if (polygon.hasLight) {
+		ctx.fillStyle = polygon.hasCycle ? "#f00" : "#0f0";
+		ctx.beginPath();
+		ctx.arc(polygon.position[0], polygon.position[1], circleSize - 1, 0, 2 * Math.PI);
+		ctx.fill();
+	}
+}
+
+let currentCanvasUpdate = 0;
+let init = false;
+let boardTransform = [5, 5];
+function initBoard() {
+	let minX = Infinity, minY = Infinity, maxX = -Infinity, maxY = -Infinity;
+	for (const [_key, poly] of game.board) {
+		poly.computeVertices();
+		for (let vertex of poly.vertices) {
+			maxX = Math.max(maxX, vertex[0]);
+			maxY = Math.max(maxY, vertex[1]);
+			minX = Math.min(minX, vertex[0]);
+			minY = Math.min(minY, vertex[1]);
+		}
+	}
+	canvas.width = (maxX - minX) + 10;
+	canvas.height = (maxY - minY) + 10;
+	ctx.resetTransform();
+	ctx.translate(5 - minX, 5 - minY);
+	boardTransform = [5 - minX, 5 - minY];
+	if (!init) renderBoard();
+	init = true;
+}
+function renderBoard() {
+	if (canvas.width > window.innerWidth) canvas.style.alignSelf = "flex-start";
+	else canvas.style.alignSelf = "center";
+	currentCanvasUpdate++;
+	for (const [_key, poly] of game.board) {
+		poly.updatePipesRotationDisplay();
+		renderPolygon(poly);
+	}
+	requestAnimationFrame(renderBoard);
+}
+
+canvas.addEventListener("click", (ev) => {
+	const x = ev.offsetX - boardTransform[0], y = ev.offsetY - boardTransform[1];
+	for (const [_, poly] of game.board) {
+		if (poly.isClicked(x, y)) {
+			if (ev.shiftKey) poly.pipesRotateAnticlockwise();
+			else poly.pipesRotateClockwise();
+			break;
+		}
+	}
+});
+
+canvas.addEventListener("contextmenu", (ev) => {
+	if (game.won) return;
+	const x = ev.offsetX - boardTransform[0], y = ev.offsetY - boardTransform[1];
+	for (const [_, poly] of game.board) {
+		if (poly.isClicked(x, y)) {
+			poly.locked = !poly.locked;
+			poly.lastCanvasUpdate = currentCanvasUpdate;
+			break;
+		}
+	}
+	ev.preventDefault();
+});
